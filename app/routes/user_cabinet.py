4cde1ab7f541e7e493208f0c4f0cc50f24537c11
curiuwu <pafnutev.vlad@gmail.com>
2025-05-03 21:24:23 +0300

    Фикс

--- app/routes/user_cabinet.py
+++ app/routes/user_cabinet.py
@@ -60,7 +60,7 @@ def get_ticket_info(user_id):
         SELECT sessions.date, sessions.time, seats.seat_number
         FROM tickets
         JOIN sessions ON tickets.session_id = sessions.session_id
-        JOIN seats ON tickets.seat_id = seats.seat_id
+        JOIN seats ON tickets.place_id = seats.seat_id
         WHERE tickets.user_id = %s        
         """, (user_id,))  # Ensure user_id is passed as a tuple
     
