799515fdff58f5685544b610ead5f3cc3090083d
curiuwu <pafnutev.vlad@gmail.com>
2025-05-19 14:45:55 +0300

    Update payment page layout and messaging; add styles for improved user experience

--- app/routes/payment.py
+++ app/routes/payment.py
@@ -2,17 +2,11 @@ from flask import Blueprint, render_template, redirect, url_for, session, reques
 from app.db import get_db_connetction
 
 payment_bp = Blueprint('payment', __name__)
-#TODO исправить оплату
+
 @payment_bp.route("/payment_page/<booking_id>")
 def confirme_payment(booking_id):
     if "user_id" not in session:
         return redirect(url_for('auth.login', next=request.url))
-    # try:
-    #     conn = get_db_connetction()
-    #     cur = conn.cursor()
-    #     cur.execute("""update table ticket_status
-
-    #     """)
     return render_template('payment.html', booking_id=booking_id)
     
     
